8646bd6f7e3690d21564889cd4675bea86f1b0dd
Jason Howell <jasonrobot64@gmail.com>
2019-02-27 20:47:49 -0800

    Restructured repo for multiple languages

---
+++ crystal/spec/s_expression_parser_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe SExpressionParser do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ crystal/spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/s_expression_parser"
