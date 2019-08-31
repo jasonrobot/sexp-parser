a2288d4996e3b9453f64697b9a68fa51b5211e12
Jason Howell <jasonrobot64@gmail.com>
2019-08-31 13:51:27 -0700

    More progress on the parser, some on the encoder. 63 assertions, 12 failures, 17 errors.

--- clojure/src/sexp_parser/core.clj
+++ clojure/src/sexp_parser/core.clj
@@ -26,8 +26,8 @@
 (defn parse-bool? [expression position]
   (cond
     (and (= (get expression position) \T)
-         (end-of-symbol? expression (+ 1 position)))
-    (parse-result true (+ 1 position))
+         (end-of-symbol? expression (inc position)))
+    (parse-result true (inc position))
     (= (subs expression position (+ 3 position)) "NIL")
     (parse-result false (+ 3 position))
     :else
@@ -36,17 +36,17 @@
 (defn parse-string? [expression position]
   (if (not (= (get expression position) \"))
     (parse-result nil position)
-    (let [start-position (+ 1 position)
-          end-position (str/index-of expression \" (+ 1 position))]
+    (let [start-position (inc position)
+          end-position (str/index-of expression \" (inc position))]
       (parse-result (subs expression start-position end-position)
-                    (+ 1 end-position)))))
+                    (inc end-position)))))
 
 ;; FIXME its keyword here, not symbol
 (defn parse-symbol? [expression position]
   (if (not (= (get expression position) \:))
     (parse-result nil position)
     (let [end-position (find-end-of-symbol expression position)]
-      (parse-result (keyword (subs expression (+ 1 position) end-position))
+      (parse-result (keyword (subs expression (inc position) end-position))
                     end-position))))
 
 (defn -parse-double [s]
@@ -59,15 +59,54 @@
       (parse-result nil position)
       (parse-result value end-position))))
 
-(defn parse-sexp? [expression position])
+;; Obviously there are no JS objects in clojure, so its a Map.
+(defn parse-object? [expression position])
 
-(defn chomp-whitespace [expression position])
+;; get the position of the next non-whitespace char
+(defn chomp-whitespace [expression start-position]
+  (def position (atom start-position))
+  (while (let [current-char (get expression @position)]
+           (or (= current-char \space)
+               (= current-char \newline)))
+    (swap! position inc))
+  @position)
 
-(defn parens-balanced? [expression])
+(defn parse-sexp? [expression position]
+  (if (not (= (get expression position) \())
+    (parse-result nil position)
+    (let [start-position (chomp-whitespace (inc position))
+          current-position (atom start-position)]
+      (while (not (= (get expression @current-position) \)))
+        (let [parsed-token (or (parse-number? expression @current-position)
+                               (parse-bool? expression @current-position)
+                               (parse-symbol? expression @current-position)
+                               (parse-string? expression @current-position)
+                               (parse-sexp? expression @current-position))]
+          ;; (swap! current-position (:next parsed-token))
+          ;; (append result (:data parsed-token)))))))
+          )))))
 
-(defn quotes-balanced? [expression])
+(defn parens-balanced? [expression]
+  (reduce (fn [acc next]
+            (cond
+              (= next \()
+              (inc acc)
+              (= next \))
+              (dec acc)))
+          0
+          expression))
 
-(defn encode-atom [data])
+(defn quotes-balanced? [expression]
+  (= 0 (mod 2 (count (re-seq #"\"" expression)))))
+
+(defn encode-atom [data]
+  (cond
+    (= false data)
+    "NIL"
+    (= true data)
+    "T"
+    :else
+    (pr-str data)))
 
 (defn encode-array [data])
 
